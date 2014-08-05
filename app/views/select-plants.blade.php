fd86451373ba6fd298185e3e46c3ec02bffb95db
bill-brikiatis <brikiatis@g.harvard.edu>
2014-08-05 09:45:30 -0400

    Change select-plants view to reflect controller logic

--- app/views/select-plants.blade.php
+++ app/views/select-plants.blade.php
@@ -2,12 +2,6 @@
 
 @section('last_frost')
 
-@if ($last_frost !=NULL)
-<h2>Your Last Frost Date is {{ $last_frost }}</h2>
-@else
-<h2>{{ "Unfortunately we did not have your postal_code in the system." }}</h2>
-@endif
-
-<p>Please select the plants you would like to grow in order to calculate your planting dates.</p>
+<h2>The Last Frost Date for Your Postal Code is {{ $last_frost }}</h2>
 
 @stop
