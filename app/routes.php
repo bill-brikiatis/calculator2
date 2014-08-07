12d127b502113367a3c77c4cf96b198cf6fcda7a
bill-brikiatis <brikiatis@g.harvard.edu>
2014-08-07 17:24:39 -0400

    Move hash up

--- app/routes.php
+++ app/routes.php
@@ -122,13 +122,15 @@ Route::post('/create-password',
         'before' => 'csrf', 
         function() {
 
-            $user = new Gardener;
+            var_dump(Hash::make(Input::get('password')));
+			die();
+
+			$user = new Gardener;
             $user->email    = Input::get('email');
             $user->password = Hash::make(Input::get('password'));
 			
             $user->gardener_Role = Input::get('gardener_Role');
-var_dump(Hash::make(Input::get('password')));
-die();
+
             # Try to add the user 
             try {
                 $user->save();
