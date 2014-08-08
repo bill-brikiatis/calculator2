0877a8380a0c13ec50cb63ea37db0e2e4ba20fdf
bill-brikiatis <brikiatis@g.harvard.edu>
2014-08-08 10:41:05 -0400

    Finish GardenTableSeeder

--- app/database/seeds/GardenerTableSeeder.php
+++ app/database/seeds/GardenerTableSeeder.php
@@ -15,6 +15,28 @@ class GardenerTableSeeder extends Seeder
 		));
 		
 		$gardener->save();
+		
+		DB::table('gardeners');
+		$gardener = new Gardener;
+		
+		$gardener->fill(array(
+				'email' => 'sylvie.brikiatis@comcast.net',
+				'password' => Hash::make('Mass'),
+		));
+		
+		$gardener->save();
+		
+		DB::table('gardeners');
+		$gardener = new Gardener;
+		
+		$gardener->fill(array(
+				'email' => 'Johanna@harvard.edu',
+				'password' => Hash::make('NH'),
+				'gardener_Role' => 'Admin'
+		));
+		
+		$gardener->save();
+		
 	}
 
 }
